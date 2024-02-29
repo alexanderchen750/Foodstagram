706cd3d1bd95069fd77e2c4d106b0fb21225919f
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-02-29 15:09:57 -0800

    Create About.js

---
+++ Frontend/src/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+/*We should probably add a navbar on all of our pages.*/
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p> 
+                This is the about page of our blog. Founded in 2023 by Elizabeth Flynn!
+            </p>
+        </div>
+
+    );
+
+}
+export default About;
