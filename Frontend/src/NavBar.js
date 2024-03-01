5bcbd157fe4705e41e30fdc8fcea2c33c77fca9b
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-02-29 18:55:56 -0800

    Update NavBar.js

--- Frontend/src/NavBar.js
+++ Frontend/src/NavBar.js
@@ -1,1 +1,18 @@
 
+// src/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link component for navigation
+import './index.css';
+const Navbar = () => {
+  return (
+    <nav className="navbar"> 
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/signin">Sign In</Link>
+    
+    </nav>
+  );
+};
+
+export default Navbar;
