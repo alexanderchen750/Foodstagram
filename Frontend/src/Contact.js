1559150ebdda95eeda503634369ec1a1d8d27bbf
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-02-29 18:54:25 -0800

    Create Contact.js

---
+++ Frontend/src/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Contact = () => {
+    return (
+    <p>Here is the base level contact info for our team: ~
+      blah-blah-blahh john smith, 
+/*if we can we can include links or button clicks to our socials.
+We could also have a contact us form, where we receieve any input. Click on link which will open email and includes our email etc..*/
+</p>
+    );
+}
+  
+  export default Contact;
+  
