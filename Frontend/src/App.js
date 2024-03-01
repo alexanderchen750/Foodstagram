4ecd8b2b398d186066a171e6940e4f324e3de34e
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-03-01 15:10:27 -0800

    Update App.js
    added search as a element

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -18,6 +18,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/signin" element={<SignIn/>} />
+          <Route path="/search" element={<Search/>} />
+
         </Routes>
       </div>
     </Router>
