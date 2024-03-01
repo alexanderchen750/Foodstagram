0648017f0f44b1f0967bfe5dfd0ab99124a1cabb
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-03-01 15:09:40 -0800

    Create Search.js
    has basic front end components for searching thru users, recies, and allows user to select however needs basic functionality and backends

---
+++ Frontend/src/Search.js
@@ -0,0 +1,96 @@
+import React, {useState} from 'react';
+/*includes a empty bar to type and checkbox
+indicating to search user or recipe, by name
+and also another version by food items or category 
+such as hallal or vegan*/
+const Search = () =>  
+{
+/*include function to search thru given data in database*/
+const[data,setData] =useState('');
+/*includes search bar to type in */
+return(
+    <div>
+      <h2>Search by users, recipes, or categories</h2>
+      <input 
+        type="text" 
+        placeholder="Search..."/>
+        <div>
+        <input 
+          type="checkbox"
+          id="users"
+          name="users"
+          value="users"
+        />
+        <label>Users</label>
+        </div>
+      <div>
+      <label>
+        <input 
+          type="checkbox"
+          id="recipeNames"
+          name="recipeNames"
+          value="recipeNames"
+        />
+        Recipe Names
+      </label>
+      </div>
+      <div>
+      <label>
+        <input 
+          type="checkbox"
+          id="vegan"
+          name="vegan"
+          value="vegan"
+        />
+        Vegan
+      </label>
+      </div>
+      <div>
+      <label>
+        <input 
+          type="checkbox"
+          id="vegetarian"
+          name="vegetarian"
+          value="vegetarian"
+        />
+        Vegetarian
+      </label>
+      </div>
+      <div>
+      <label>
+        <input 
+          type="checkbox"
+          id="glutenFree"
+          name="glutenFree"
+          value="glutenFree"
+        />
+        Gluten-Free
+      </label>
+      </div>
+      <div>
+      <label>
+        <input 
+          type="checkbox"
+          id="keto"
+          name="keto"
+          value="keto"
+        />
+        Keto
+      </label>
+      </div>
+      <div>
+      <label>
+        <input 
+          type="checkbox"
+          id="halal"
+          name="halal"
+          value="halal"
+        />
+        Halal
+      </label>
+      </div>
+        <button type="submit">Search</button>
+    </div>
+);
+}
+export default Search;
