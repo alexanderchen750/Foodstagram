7d42a50380661d0a383b15cb618301924e1512a5
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-03-01 00:54:31 -0800

    Update SignIn.js
    added visual components such as sign in header, button, and typing forms. backend needs work, needs functions

--- Frontend/src/SignIn.js
+++ Frontend/src/SignIn.js
@@ -1,6 +1,42 @@
-/*redirects to sign in page.*/
+import React, { useState } from 'react';
+import ReactDom from "react-dom";
+//database to check if email and password is in database
+
+//function to submit data!
+
+// probably set a global bool to indicate signed in state.
+// if so, set var.
 const SignIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  return (
+    <form>
+    <h2> Sign In </h2>
+    <div>
+        <label> Email: </label> 
+        <input 
+            type="email"
+            id="email" 
+            value={email} 
+            required 
+        />
+    </div>
+    <div>
+          <label> Password: </label>
+            <input 
+                type="password" 
+                id="password" 
+                value={password} 
+                required 
+            />
+         <br/>             
+        <button type="submit">Sign In</button>
+        <br/>
+          <button type="submit">Register</button>
+      </div>
+  </form>
 
-
+  );
 }
-export default SignIn
+
+export default SignIn;
