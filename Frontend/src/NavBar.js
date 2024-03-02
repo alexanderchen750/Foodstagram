2c45c49285488132ef684e7bd32c28af49ce721b
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-03-01 20:10:31 -0800

    Update NavBar.js

--- Frontend/src/NavBar.js
+++ Frontend/src/NavBar.js
@@ -1,19 +1,23 @@
-
 // src/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link component for navigation
 import './index.css';
 const Navbar = () => {
   return (
-    <nav className="navbar"> 
+/*below is the header  for the navbar and links*/
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">     
+  <nav className="navbar">
+          <Link to="/" className="navbar-brand">Foodagram</Link>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/signin">Sign In</Link>
           <Link to="/search">Search</Link> 
-
+    </nav>
+    </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
