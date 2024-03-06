9c4110bbac71e5a346e2d1723a9f3b2ed0f60696
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-03-05 20:22:31 -0800

    Create Post.js

---
+++ Frontend/src/Post.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import "./Post.css"
+import { Avatar } from '@mui/material'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+
+
+
+function Post({user, postImage, likes, timestamp, blogtext}) {
+  return (
+    <div className="post">
+        <div className="post__header">
+            <div className="post__headerAuthor">
+            <Avatar className="avatar">{user.charAt(0).toUpperCase()}</Avatar>
+            {user} • <span className="avatar">{timestamp}</span>
+            </div>
+            <MoreHorizIcon className="threeDots"/>
+        </div>
+        <div className="post__image">
+            <img
+            src={postImage}
+            alt=""
+            />
+        </div>
+        <div className="post__blogtext">
+            {blogtext}
+        </div>
+        <div className="post__footer">
+            <div className="post__footerIcons">
+                <div className="post__iconsMain">
+                    <FavoriteBorderIcon className="postIcon"/>
+                    <ChatBubbleOutlineIcon className="postIcon"/>
+                    <TelegramIcon className="postIcon"/>
+
+                </div>
+                <div className="post__iconSave">
+                    <BookmarkBorderIcon className="postIcon"/>
+                </div>
+
+            </div>
+            Liked by {likes} people.
+        </div>
+    </div>
+  )
+}
+
+export default Post
