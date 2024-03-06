1f3cc4a6897a92be4748463d1d335416f3aa1deb
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-03-05 23:27:06 -0800

    Update About.js

--- Frontend/src/About.js
+++ Frontend/src/About.js
@@ -1,33 +1,35 @@
-import React from 'react';
-/*format this to look more friendly and maybe incorporate a friendly image*/
-const About = () => {
-    return (
-        <div>
-            <h1>About us</h1>
-            <p> 
-            <img src="https://i.imgur.com/UPd0zLS.jpeg" alt="Description" style={{ width: '600px', height: '400px' }} />;
-        <hr/>
-        <b>Introduction to Foodagram - </b>
-            Welcome to Foodagram - Your ultimate destination for exploring, sharing, and celebrating the art of cooking and the joy of eating. 
-            Foodagram is a community-driven platform that brings together food enthusiasts from around the globe. 
-            Whether you're a professional chef, a home cook, or someone who just loves food, Foodagram is your space to share your culinary 
-            creations, discover new recipes, and connect with fellow food lovers.
-        <hr/>
-        <h3>Our Mission</h3> 
-           <h3> Empowering Culinary Creativity</h3>At Foodagram, we believe that cooking is an art form that thrives on creativity, experimentation, and sharing. Our mission is to create a welcoming space where anyone can share their love for food, discover new flavors, and learn from the diverse culinary traditions of our global community.
-            Features
+import React from 'react'
+import "./About.css";
+function About() {
+  return (
+            <div className="about">
+                <div className="about-text">                
+                <h1>About us</h1>
+                <p> 
+                <img src="https://i.imgur.com/UPd0zLS.jpeg" alt="Description" style={{ width: '600px', height: '400px' }} />;
+            <hr/>
+            <b>Introduction to Foodagram - </b>
+                Welcome to Foodagram - Your ultimate destination for exploring, sharing, and celebrating the art of cooking and the joy of eating. 
+                Foodagram is a community-driven platform that brings together food enthusiasts from around the globe. 
+                Whether you're a professional chef, a home cook, or someone who just loves food, Foodagram is your space to share your culinary 
+                creations, discover new recipes, and connect with fellow food lovers.
+            <hr/>
+            <h3>Our Mission</h3> 
+               <h3> Empowering Culinary Creativity</h3>At Foodagram, we believe that cooking is an art form that thrives on creativity, experimentation, and sharing. Our mission is to create a welcoming space where anyone can share their love for food, discover new flavors, and learn from the diverse culinary traditions of our global community.
+                Features
+            
+                <h3>Discover & Share Recipes </h3>Browse through a vast collection of recipes shared by our community. Whether you're looking for 
+                vegan dishes, traditional cuisines, or the latest culinary trends, Foodagram has something for everyone.
+                <h3>Connect with Food Enthusiasts</h3>Follow your favorite cooks, share your culinary adventures, and join discussions. 
+                Foodagram is more than a recipe sharing platform; it's a place to connect and grow with like-minded individuals.
+                <h3>Learn from the Best </h3>Participate in cooking challenges, watch tutorial videos, and read blogs from culinary experts. 
+                Foodagram is your go-to resource for culinary knowledge and inspiration.
+    
+                </p>
+                </div>
+            </div>
         
-            <h3>Discover & Share Recipes </h3>Browse through a vast collection of recipes shared by our community. Whether you're looking for 
-            vegan dishes, traditional cuisines, or the latest culinary trends, Foodagram has something for everyone.
-            <h3>Connect with Food Enthusiasts</h3>Follow your favorite cooks, share your culinary adventures, and join discussions. 
-            Foodagram is more than a recipe sharing platform; it's a place to connect and grow with like-minded individuals.
-            <h3>Learn from the Best </h3>Participate in cooking challenges, watch tutorial videos, and read blogs from culinary experts. 
-            Foodagram is your go-to resource for culinary knowledge and inspiration.
-
-            </p>
-        </div>
-
-    );
-
+  );
 }
-export default About;
+
+export default About
