2d50816ba68d2bd706f3f50a06c6d5806868bd62
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-03-06 01:38:02 -0800

    Update Homepage.js

--- Frontend/src/Homepage.js
+++ Frontend/src/Homepage.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
 import "./Homepage.css";
 import Timeline from './timeline/Timeline';
+import Sidenav from './navigation/Sidenav';
+
 function Homepage() {
   return (
-    <div className="homepage__timeline">
+    <div className='homepage'>
+      <div className='homepage__nav'>
+      </div>
+      <div className="homepage__timeline">
         <Timeline />
+        </div>
     </div>
   );
 }
