949ff361582fbdbc576ce0a6d7dc920bc0d87c44
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-03-05 23:27:42 -0800

    Update App.js
    links now work but for some reason visuals are bad! needs work

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,37 @@
 import './App.css';
+import Sidenav from './navigation/Sidenav';
+import { BrowserRouter as Router, 
+  Route, Routes } from 'react-router-dom';
+import About from './navigation/About';
 import Homepage from './Homepage';
-
-/*import Home from './Home';
 import Search from './Search';
 import Contact from './Contact';
-import About from './About';
 import Register from './Register';
-import Login from './Login';
-import Logout from './Logout';
-*/
+import SignIn from './Login';
+import LogOut from './Logout';
+
+import "./Homepage.css"
 function App() {
+
   return (
-    <div className="App">
-      <Homepage />
-    </div>
+    
+    <Router>
+      <div className ="homepage">
+       <Sidenav/>      
+        <Routes>
+            <Route path="/" element={<Homepage />} />  
+            <Route path="/about" element={<About />} />  
+            <Route path="/search" element={<Search />} />  
+            <Route path="/contact" element={<Contact />} />  
+            <Route path="/register" element={<Register />} />  
+            <Route path="/login" element={<SignIn />} /> 
+            <Route path="/logout" element={<LogOut />} />  
+
+    </Routes> 
+      </div>
+
+    </Router>
+        
   );
 }
 
