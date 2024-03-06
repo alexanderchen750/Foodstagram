cdd3a0c4269d45c86dc84cdf30cb18565cca77f2
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2024-03-05 20:17:16 -0800

    Update App.js
    router will be placed in homepage.js instead, calls homepage

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,29 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BlogList from './BlogList';
-import BlogPost from './BlogPost';
-import Home from './Home';
-import NavBar from './NavBar';
-import About from './About';
-import SignIn from './SignIn';
+import './App.css';
+import Homepage from './Homepage';
+
+/*import Home from './Home';
+import Search from './Search';
 import Contact from './Contact';
-/*this imports from the .js files^ and the bottom portion essentially makes a navbar on every page.*/
+import About from './About';
+import Register from './Register';
+import Login from './Login';
+import Logout from './Logout';
+*/
 function App() {
   return (
-    <Router>
-      <div>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={<navbar-brand />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/signin" element={<SignIn/>} />
-          <Route path="/search" element={<Search/>} />
-
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Homepage />
+    </div>
   );
 }
 
