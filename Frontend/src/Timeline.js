1175f3a5793400d2801d29c144eb611df5c8137f
alexanderchen750 <alexander.chen750@gmail.com>
2024-03-10 11:20:09 -0700

    Fetching Backend Posts to the Timeline If the user has the backend and mongodb set up properly, then the timeline should be displaying posts. The pictures aren't working because I need to figure how to store pictures, mongodb may not be the best for that. But the changes are mainly to timeline.js, where I added the fetch portino. Also need to update you're package.json and install cors in the backend folder for this to work

--- Frontend/src/Timeline.js
+++ Frontend/src/Timeline.js
@@ -1,12 +1,60 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import "./Timeline.css"
 import Suggestions from './Suggestions.js'
 import Post from './Post.js'
 
 
+
 function Timeline() {
-  //const [posts, setPost] = useState([
-  const [posts, ] = useState([
+  const [posts, setPosts] = useState(null); // Initialized state
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+
+      const response = await fetch('/api/posts');
+      const json = await response.json();
+      console.log("done waiting")
+      if (response.ok) {
+        setPosts(json);
+        console.log("sucess");
+      }
+      else
+      {
+        console.log("Fail to fetch posts to timeline");
+      }
+    };
+
+    fetchPosts(); // Call the function to fetch posts
+  }, []); // Dependency array to ensure effect runs only once
+
+  return (
+    <div className='timeline'>
+      <div className="timeline__left">
+        <div className="timeline__posts">
+          {posts ? posts.map(post => ( // Check if posts is not null
+            <Post 
+              key={`${post.user}_${post.timestamp}`} 
+              user={post.user}
+              postImage={post.postImage} 
+              likes={post.likes} 
+              timestamp={post.timestamp}
+              blogtext={post.blogtext}
+            />
+          )) : "Loading..."} {/* Display loading or a message when posts are null */}
+        </div>
+      </div>
+      <div className="timeline__right">
+          <Suggestions />
+      </div>
+  </div>
+  );
+}
+
+export default Timeline;
+
+
+/*const [posts, ] = useState([
     {
       user: "LemonChickenBoss",
       postImage: "https://christieathome.com/wp-content/uploads/2022/03/Chinese-Lemon-Chicken-32.jpg",
@@ -35,29 +83,4 @@ function Timeline() {
       timestamp: "42m",
       blogtext: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis egestas maecenas pharetra convallis posuere morbi. Habitant morbi tristique senectus et netus et. Integer eget aliquet nibh praesent. Maecenas accumsan lacus vel facilisis volutpat est velit egestas dui. Vitae turpis massa sed elementum tempus egestas sed sed. Quam adipiscing vitae proin sagittis nisl rhoncus. Tellus mauris a diam maecenas sed. Enim ut tellus elementum sagittis vitae. Faucibus et molestie ac feugiat sed lectus vestibulum mattis. Arcu dui vivamus arcu felis bibendum ut.",
     },
-  ]);
-
-  return (
-    <div className='timeline'>
-        <div className="timeline__left">
-          <div className="timeline__posts">
-            {posts.map(post => (
-              <Post 
-                key={`${post.user}_${post.timestamp}`} 
-                user={post.user}
-                postImage={post.postImage} 
-                likes={post.likes} 
-                timestamp={post.timestamp}
-                blogtext={post.blogtext}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="timeline__right">
-            <Suggestions />
-        </div>
-    </div>
-  )
-}
-
-export default Timeline
+  ]);*/
