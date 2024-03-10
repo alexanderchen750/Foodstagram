5049a26100eb4e57b477eee9a04f626775cafa1f
Alsadegzahra <160082410+Alsadegzahra@users.noreply.github.com>
2024-03-09 22:58:34 -0800

    Update Register.js
    user can add username

--- Frontend/src/Register.js
+++ Frontend/src/Register.js
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 
 const Register = () => {
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
     };
@@ -11,12 +15,22 @@ const Register = () => {
     };
     const handleSubmit = (event) => {
         event.preventDefault(); // Prevents the default form submit action
-        console.log('Registering with:', email, password);
+        console.log('Registering with:',username, email, password);
         // Here, you would typically dispatch these values to a backend service for registration
     };
     return (
     <form onSubmit={handleSubmit}>
     <h2> Register</h2>
+    <div>
+        <label> Username: </label> 
+        <input 
+            type="username"
+            id="username" 
+            value={username} 
+            onChange={handleUsernameChange}
+            required 
+        />
+    </div>
     <div>
         <label> Email: </label> 
         <input 
