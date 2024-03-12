17c444f1c1e6edb9b1269aac4d2ced446fde1076
alexanderchen750 <alexander.chen750@gmail.com>
2024-03-11 21:05:30 -0700

    update Timeline

--- Backend/src/models/postModel.js
+++ Backend/src/models/postModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const postSchema = new Schema({
-    user: {type:String, required: true},//{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: {type:String},//{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     blogtext: String,
     postImage: String,
     timestamp: { type: Date, default: Date.now },
@@ -21,7 +21,7 @@ const postSchema = new Schema({
 
 
 //For the Search
-postSchema.index({ title: 'text', body: 'text' });
+postSchema.index({ blogtext: 'text', tags: 'text', ingredients: 'text' });
 
 module.exports = mongoose.model('RecipePosts', postSchema)
 
