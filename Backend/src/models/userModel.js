85c1abc8b8b9563dc1ec656b348fb392825666da
alexanderchen750 <alexander.chen750@gmail.com>
2024-03-13 17:00:55 -0700

    Updating Backend Files to Support Password Encryption and JWTokens Finished most of backend work surrounding account and security. Use Bcrypt Package to encrypt the passwords stored in the backend incase of database breach. Use jwt package to generate tokens to send to frontend that represent user logged in status. Now, I'm currently modifying the front end to support logged in status

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
+const validator = require('validator')
+
 
 const userSchema = new Schema({
     username: { type: String, required: true, unique: true },
@@ -9,12 +11,56 @@ const userSchema = new Schema({
     bio: String,
     favoritedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
     directMessages: [{
-      sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-      message: String,
-      createdAt: { type: Date, default: Date.now }
+        sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        message: String,
+        createdAt: { type: Date, default: Date.now }
     }]
   });
   
-  //const User = mongoose.model('User', userSchema);
+//const User = mongoose.model('User', userSchema);
+userSchema.statics.signup = async function(username, email, password){
+    if(!email || !password || !username){
+        throw Error("All Fields must be filled")
+    }
+    if(!validator.isEmail(email)){
+        throw Error("Invalid Email")
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error("Password Must be 8+ Letter Combo of Lowercase, Uppercase, Numbers, and Symbols")
+    }
+    const emailExists = await this.findOne({email})
+    if(emailExists){
+      throw Error("email already in use")
+    }
+    const usernameExists = await this.findOne({username})
+    if(usernameExists){
+      throw Error("username already in use")
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({username, email, password: hash})
+
+    return user
+}
+
+userSchema.statics.login = async function(username, password){
+    if(!password || !username){
+        throw Error("All Fields must be filled")
+    }
+    const usernameExists = await this.findOne({username})
+    if(!usernameExists){
+      throw Error("Username Does Not Exist")
+    }
+
+    const match = await bcrypt.compare(password, usernameExists.password)
+    if(!match){
+        throw Error("Incorrect Password or Username")
+    }
+    return usernameExists
+}
+
+
 
   module.exports = mongoose.model('userPosts', userSchema)
